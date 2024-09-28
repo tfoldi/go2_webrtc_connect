75048178082b1f8158045f57879d5a637a2f5d03
Tamas Foldi <tfoldi@xsi.hu>
2024-09-28 09:53:05 +0200

    fix: move imshow to main thread

--- examples/video/camera_stream/display_video_channel.py
+++ examples/video/camera_stream/display_video_channel.py
@@ -5,36 +5,36 @@ from aiortc import MediaStreamTrack
 from aiortc.contrib.media import MediaRecorder
 import cv2
 import os
+import queue
 
 # Enable logging for debugging
 logging.basicConfig(level=logging.FATAL)
 
+imgq = queue.Queue(10)
+
 # This function receives video frames from the camera stream
 async def recv_camera_stream(track: MediaStreamTrack):
+    global imgq
+
     while True:
-        print("Frame")
         frame = await track.recv()
 
         # Convert the video frame to a NumPy array in BGR format (for OpenCV)
         img = frame.to_ndarray(format="bgr24")
 
         print(f"Shape: {img.shape}, Dimensions: {img.ndim}, Type: {img.dtype}, Size: {img.size}")
-
-        # Show the frame on the main thread
-        cv2.imshow('Camera Stream', img)
-
-        # OpenCV requires a small delay; this also checks for 'q' to quit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        # To avoid blocking, yield control to the asyncio loop for a moment
-        await asyncio.sleep(0)
+        try:
+            imgq.put_nowait(img)
+        finally:
+            pass
 
     # When the loop ends, close the OpenCV window
     cv2.destroyAllWindows()
 
 # Main function for setting up the WebRTC connection and handling streams
 async def main():
+    global img
+
     try:
         # Choose a connection method (uncomment the correct one)
         conn = Go2WebRTCConnection(WebRTCConnectionMethod.LocalSTA, ip=os.getenv('ROBOT_IP', '192.168.8.181'))
@@ -51,8 +51,18 @@ async def main():
         # Add callback to handle received video frames (track)
         conn.video.add_track_callback(recv_camera_stream)
 
-        # Keep the program running 
-        await asyncio.sleep(3600) 
+        while True:
+            try:
+                img = imgq.get_nowait()
+
+                # Show the frame on the main thread
+                cv2.imshow('Camera Stream', img)
+
+                # OpenCV requires a small delay; this also checks for 'q' to quit
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            except:
+                await asyncio.sleep(0.01)
 
     except ValueError as e:
         logging.error(f"Error in WebRTC connection: {e}")
